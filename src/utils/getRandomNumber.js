00e4193bf82cd420aa452026fbac6bd2e8306477
Jose Gregorio Brazon Lopez <94471674+brazon84@users.noreply.github.com>
2023-03-15 21:30:02 -0300

    correccion

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,6 +1,5 @@
 export const getRandomNumber=(min, max)=>{
     const maxMin = max-min;
     const maxMinRandom = Math.random()* maxMin;
-    console.log(min + Math.round(maxMinRandom))
     return min + Math.round(maxMinRandom);
 }
